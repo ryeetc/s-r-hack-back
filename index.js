b03e6f79b809d3e46159a703c7739029eaf5bc58
simon codes <simont.codes@gmail.com>
2022-08-31 22:54:45 -0400

    made it so you cannot get 2 movies with same rating

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express = require("express")
-const axios = require("axios")
-const cors = require("cors")
-const fs = require("fs")
-const app = express()
-app.use(cors())
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const fs = require("fs");
+const app = express();
+app.use(cors());
 
- app.get("/videos", (req,res) => {
-    const readFile = fs.readFileSync("./movies2.json");
-    const videoFile = JSON.parse(readFile);
-    let randomNum1 = Math.floor(Math.random() * 250)
-    let randomNum2 = Math.floor(Math.random() * 250)
-    let randomVideos = [videoFile[randomNum1],videoFile[randomNum2]]
-    res.send(randomVideos)
- })
+function isSameRating(movie1, movie2) {
+  while (movie1.rating === movie2.rating) {
+    let randomNum1 = Math.floor(Math.random() * 250);
+    let randomNum2 = Math.floor(Math.random() * 250);
+  }
+}
+
+app.get("/videos", (req, res) => {
+  const readFile = fs.readFileSync("./movies2.json");
+  const videoFile = JSON.parse(readFile);
+  let randomNum1 = Math.floor(Math.random() * 250);
+  let randomNum2 = Math.floor(Math.random() * 250);
+
+  // ---------------------------------------------------------------------
+  while (videoFile[randomNum1].rating === videoFile[randomNum2].rating) {
+    randomNum1 = Math.floor(Math.random() * 250);
+    randomNum2 = Math.floor(Math.random() * 250);
+  }
+
+  // -----------------------------------------------------------------------
+  let randomVideos = [videoFile[randomNum1], videoFile[randomNum2]];
+  console.log(randomVideos[0].rating, randomVideos[1].rating);
+  res.send(randomVideos);
+});
 
 // axios.get("https://imdb-api.com/en/API/Top250Movies/k_6ew1h179")
 //     .then((response)=>{
@@ -31,6 +47,6 @@ app.use(cors())
 //         }
 //         fs.writeFileSync("./movies2.json", JSON.stringify(ourVideos))
 //     })
-app.listen(8080, ()=>{
-    console.log("I'm here and I'm listening on port 8080")
-})
+app.listen(8080, () => {
+  console.log("I'm here and I'm listening on port 8080");
+});
